279e00bc85b8bd129dcb769c91a506b3363cac57
reginaldo-pfr <reginaldo.fernands@gmail.com>
2025-01-27 17:56:03 -0300

    Configuração base e partials templates

--- stockly/views.py
+++ stockly/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # View para a página inicial
 def home(request):
-    return render(request, 'home.html')  
+    return render(request, 'stockly/pages/home.html')  
